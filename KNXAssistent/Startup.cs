954e7964fa94c978922c64f0bcac4f1b60ea1775
VicentNedelko <70284853+VicentNedelko@users.noreply.github.com>
2021-11-25 17:57:53 +0300

    feat : improve BotSettings razor

--- KNXAssistent/Startup.cs
+++ KNXAssistent/Startup.cs
@@ -45,6 +45,7 @@ namespace KNXAssistent
               })
               .AddBootstrapProviders()
               .AddFontAwesomeIcons();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
